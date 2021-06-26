f5a248791f9aaa7f269133baccadc7f6fde1704b
xxf <1810606106@qq.com>
2021-06-26 22:25:08 +0800

    第一次提交项目

---
+++ src/com/jt/pojo/Hello.java
@@ -0,0 +1,13 @@
+package com.jt.pojo;
+
+
+
+public class Hello {
+    public static void main(String[] args) {
+
+        for (int i=0;i<10;i++){
+            System.out.println(i);
+        }
+
+    }
+}
